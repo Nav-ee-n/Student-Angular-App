04f9f81c2bf78d1b76aa03cd8c08462e01c93cdd
Nav-ee-n <naveenfelix876@gmail.com>
2022-07-08 11:35:39 +0530

    Updated view-student

--- src/app/view-students/view-students.component.ts
+++ src/app/view-students/view-students.component.ts
@@ -12,6 +12,13 @@ export class ViewStudentsComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  name="John"
+  data=[
+    {"name":"Sudhil","age":19,"photo":"https://images.vexels.com/media/users/3/145908/raw/52eabf633ca6414e60a7677b0b917d92-male-avatar-maker.jpg"},
+    {"name":"Naveen","age":21,"photo":"https://images.vexels.com/media/users/3/145908/raw/52eabf633ca6414e60a7677b0b917d92-male-avatar-maker.jpg"},
+    {"name":"Abner","age":23,"photo":"https://images.vexels.com/media/users/3/145908/raw/52eabf633ca6414e60a7677b0b917d92-male-avatar-maker.jpg"},
+    {"name":"Peter","age":45,"photo":"https://images.vexels.com/media/users/3/145908/raw/52eabf633ca6414e60a7677b0b917d92-male-avatar-maker.jpg"},
+    {"name":"John","age":89,"photo":"https://images.vexels.com/media/users/3/145908/raw/52eabf633ca6414e60a7677b0b917d92-male-avatar-maker.jpg"},
+    {"name":"Haine","age":32,"photo":"https://images.vexels.com/media/users/3/145908/raw/52eabf633ca6414e60a7677b0b917d92-male-avatar-maker.jpg"}
+  ]
 
 }
